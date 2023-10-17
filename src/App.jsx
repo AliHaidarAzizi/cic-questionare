b17cfe61a2c33ade9b9588cc5ae998e785bf0795
AliHaidarAzizi <135135657+AliHaidarAzizi@users.noreply.github.com>
2023-10-17 09:54:00 +0800

    settle with form design and axios configuration

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Questionnaire from './component/questionare.jsx'; // Import the Questionn
 
 function App() {
   const [showQuestionnaire, setShowQuestionnaire] = useState(false);
-
+  const [language, setLanguage] = useState('en')
   const handleClick = () => {
     setShowQuestionnaire(true);
   };
 
+  const handleLanguageToggle = () => {
+    setLanguage(language === 'en' ? 'bm' : 'en');
+  };
+  
   return (
     <>
       <div className='bg-white min-h-screen flex flex-col items-center justify-center font-sans gap-5 p-4'>
-        <div className='bg-slate-200 min-[700px]:w-2/3 md:box-content flex flex-col items-center justify-center gap-3 h-[700px] p-4 rounded-3xl text-center shadow-md'>
+        <div className='bg-slate-200 min-[700px]:w-2/3 md:box-content flex flex-col items-center justify-center gap-3 h-[700px] p-8 rounded-3xl text-center shadow-md'>
             {!showQuestionnaire ? (
               <div className='flex flex-col gap-5'>
                 <h1 className="text-3xl font-bold"> Welcome to CIC's Personality Test! </h1>
@@ -24,8 +28,13 @@ function App() {
                 </div>
               </div>
               ) : (
-              <Questionnaire />
+              <Questionnaire language={language} />
               )}
+          <div className=' mt-8'>
+            <button onClick={handleLanguageToggle}>
+               Switch to {language === 'en' ? 'Bahasa Malaysia' : 'English'}
+               </button>
+          </div>
         </div>
       </div>
     </>
--- src/component/questionare.jsx
+++ src/component/questionare.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { questions } from './questions';
+import axios from 'axios';
 
 
-function Questionnaire() {
+function Questionnaire({ question, language }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState([]);
   const [email, setEmail] = useState("");
@@ -22,28 +23,44 @@ function Questionnaire() {
     setName(event.target.value);
   };
 
-  const handleSubmit = () => {
-    // Here you can handle the submission of the email and score
-    // For example, you might send them to a server
-    console.log("Name:", Name);
-    console.log("Email:", email);
-    console.log("Total Score:", totalScore);
+  const handleSubmit = async (e) => {
+    const Name = e.target[0].value;
+    const gender = e.target[1].value;
+    const doB = e.target[2].value;
+    const email = e.target[3].value;
+    const reqBody = { Name, gender, doB, email, totalScore };
+    console.log(e.target)
+    debugger
+    try {
+      const res = await axios.post("http://localhost:3000/submission", reqBody)
+      alert(`your score is: ${totalScore}`)
+    } catch (error) {
+      console.error(error);
+      alert(error.response.data.message)
+      
+    }
   };
+
 
   if (currentQuestionIndex >= questions.length) {
     return (
       <div className='flex flex-col items-center '>
         <h2 className='text-3xl font-bold py-3 my-3'>Enter your details to see your total score:</h2>
-        <form action="" method="post" className='flex flex-col gap-2'>
-            <input placeholder=' Name' className=' h-10 rounded-lg w-60' type="Name" value={Name} onChange={handleNameInput} />
+        <form action="" onSubmit={(event) => handleSubmit(event)} className='flex flex-col gap-2'>
+          {/* name */}
+            <input placeholder='Name' className=' p-2 h-10 rounded-lg w-60' type="Name" value={Name} name='Name' id='Name' onChange={handleNameInput} />
+            {/* Gender */}
                 <select id="gender" className='h-10 rounded-lg' name="gender">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 </select>
+            {/* date of birth */}
             <label htmlFor="DoB">Date of Birth</label>
-            <input className=' h-10 rounded-lg w-60' type="date" min="1960-01-01" max="2010-01-01" />
-            <input placeholder=' Email' className=' h-10 rounded-lg w-60' type="email" value={email} onChange={handleEmailInput} />
-            <button className='max-[300px]: bg-purple-600 text-white py-2 px-4 rounded-lg m-3' onClick={handleSubmit}>Submit</button>
+            <input className=' h-10 rounded-lg w-60' name='doB' id='doB' type="date" min="1960-01-01" max="2010-01-01" />
+            {/* email */}
+            <input placeholder='Email' className=' p-2 h-10 rounded-lg w-60' type="email" value={email} name='email' id='email' onChange={handleEmailInput} />
+
+            <button className='max-[300px]: bg-purple-600 shadow-lg text-white py-2 px-4 rounded-lg m-3' type='submit'>Submit</button>
 
         </form>
       </div>
@@ -52,10 +69,20 @@ function Questionnaire() {
 
   return (
     <div className='flex flex-col gap-2'>
-      <h2 className='text-3xl font-bold py-3'>{questions[currentQuestionIndex].question}</h2>
+    <h3>{currentQuestionIndex+1}/{questions.length}</h3>
+      <h2 className='text-3xl font-bold py-3 mb-4'>{language === 'en' ? questions[currentQuestionIndex].question : questions[currentQuestionIndex].translation}</h2>
       {questions[currentQuestionIndex].options.map((option, index) => (
-        <button className='max-[300px]: bg-purple-600 text-white py-2 px-4 rounded-lg' key={index} onClick={() => handleAnswer(questions[currentQuestionIndex].scores[index])}>
-          {option}
+        <button 
+        className='
+          max-[300px]: bg-purple-600
+          text-white
+          shadow-lg
+          py-2 px-4 
+           hover:bg-purple-900
+          rounded-lg' 
+    
+         key={index} onClick={() => handleAnswer(questions[currentQuestionIndex].scores[index])}>
+          {language === 'en' ? option : questions[currentQuestionIndex].translationOptions[index]}
         </button>
       ))}
     </div>
